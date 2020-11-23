6ac201b5e1ee0e32437d81a7db892f051cbd9f97
Brigitte <u201914955@upc.edu.pe>
2020-11-23 00:28:30 -0500

    astar-net

--- code/algorithms.py
+++ code/algorithms.py
@@ -1,4 +1,6 @@
 import networkx as nx
+import heapq as hp
+
 def dijkstra(G, start, goal):
 
         grafo = nx.to_dict_of_lists(G)
@@ -45,7 +47,52 @@ def dijkstra(G, start, goal):
             else:
                 return S
         
-def astar(G, origin, goal):
-    return nx.astar_path(G, origin, goal)
+def astar(G, start, goal):
+    return nx.astar_path(G, start, goal)
+    def heuristic(current, goal):
+        h = abs(current[0] - goal[0]) + abs(current[1] - goal[1])
+        return float(h)
+
+    def cercania(nodo1, nodo2):
+        resta = nodo1 - nodo2
+        lista = [-1,1, size, -size]
+        if resta in lista:
+            return True
+
+    inf = float("inf")
+    tam_grafo = G.number_of_nodes()
+    size = tam_grafo ** 0.5
+    local = [inf] * tam_grafo
+    visitado = [False] * tam_grafo
+    local[start] = 0.0
+    c = start % size + start //size
+    f = start // size
+    c1 = goal % size + goal // size
+    f1 = goal // size
+    current = start
+    ruta = []
+    q = [(heuristic((c, f), (c1, f1)), 0.0, False, start, start)]
+    ruta.append(start)
+    while len(q) and current!=goal :
+
+        g, l, v, current, papa = hp.heappop(q)
+        if visitado[current]: continue
+        if ruta.count(papa)==0 and cercania(papa, ruta[-1]):
+            ruta.append(papa)
+        visitado[current] = True
+        for neighbor in G.neighbors(current):
+            sum = float(local[current] + 1)
+            if (not visitado[neighbor]) and sum < local[neighbor]:
+                local[neighbor] = sum
+                c = neighbor % size + neighbor // size
+                f = neighbor // size
+                hp.heappush(q, (local[neighbor] + heuristic((c, f), (c1, f1)), sum, visitado[neighbor], neighbor, current))
+    if len(q) == 0 and current!=goal:
+        return []
+
+    ruta.append(goal)
+    return ruta
+    
+
 def bellman_ford(G, origin, goal):
     return nx.bellman_ford_path(G, origin, goal)
